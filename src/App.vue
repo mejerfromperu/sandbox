315e08a62358707f0b17eeb3d320f215f7845e68
mejerfromperu <christianmejer7@gmail.com>
2026-03-29 21:45:55 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+import { ref } from "vue";
+
+
+const testname = ref("test name ");
+
+
+</script>
 
 <template>
   <h1>You did it!</h1>
+
+  <h1>{{ testname }}</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
