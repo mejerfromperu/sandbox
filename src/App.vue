b1e02dcd76453eb038b264adb414aee4c63a0b6a
mejerfromperu <christianmejer7@gmail.com>
2026-03-29 21:59:06 +0200

    initial commit 2

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,17 @@
 import { ref } from "vue";
 
 
+
 const testname = ref("test name ");
+const counter = ref<number>(0);
+
+
+function add() : number {
+  counter.value += 1;
+  return counter.value;
+}
+
+
 
 
 </script>
@@ -12,10 +22,23 @@ const testname = ref("test name ");
   <h1>You did it!</h1>
 
   <h1>{{ testname }}</h1>
+  <button @click="add">Add</button>
+  <p>counter: {{ counter }}</p>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+.button {
+  background-color: #42b983;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+</style>
