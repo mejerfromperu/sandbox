db8d9763e41e5f83d7dee7d7e7b59b1fb3b9c491
mejerfromperu <christianmejer7@gmail.com>
2026-03-30 21:54:18 +0200

    initial commit 3

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,58 @@
 <script setup lang="ts">
 
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 
+  const store = reactive({
+  selectedId: 0,
+  selectedTitle: "",
+  selectedCompany: "",
+  selectedLocation: "",
+  selectedDescription: "",
+  job: {
+    id: 0,
+    title: "",
+    company: "",
+    location: "",
+    description: "",
+  },
+  
+});
 
+export type Job = {
+  id: number;
+  title: string;
+  company: string;
+  location: string;
+  description: string;
+};
 
-const testname = ref("test name ");
-const counter = ref<number>(0);
+const jobs = ref<Job[]>([]);
 
+function onClickJob() {
+  const newJob: Job = {
+    id: store.selectedId,
+    title: store.selectedTitle,
+    company: store.selectedCompany,
+    location: store.selectedLocation,
+    description: store.selectedDescription,
+  };
+
+  jobs.value.push(newJob);
+
+  store.selectedId = 0;
+  store.selectedTitle = "";
+  store.selectedCompany = "";
+  store.selectedLocation = "";
+  store.selectedDescription = "";
+}
+
+const counter = ref(0);
+const testname = "Vue 3 + TypeScript";
 
 function add() : number {
   counter.value += 1;
   return counter.value;
 }
-
-
-
 
 </script>
 
@@ -28,9 +66,23 @@ function add() : number {
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <form @submit.prevent class="job-form">
+    <input type="number" v-model="store.selectedId" placeholder="Job ID" />
+    <input type="text" v-model="store.selectedTitle" placeholder="Job Title" />
+    <input type="text" v-model="store.selectedCompany" placeholder="Company" />
+    <input type="text" v-model="store.selectedLocation" placeholder="Location" />
+    <textarea v-model="store.selectedDescription" placeholder="Description"></textarea>
+    <button type="button" @click="onClickJob" class="button">Add Job</button>
+  </form>
+  <p v-for="job in jobs" :key="job.id">
+    <strong>{{ job.title }}</strong> at {{ job.company }} in {{ job.location }}
+    <br />
+    {{ job.description }}
+  </p>
 </template>
 
 <style scoped>
+
 
 .button {
   background-color: #42b983;
